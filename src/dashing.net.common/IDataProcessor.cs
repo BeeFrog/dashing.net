68cab25f37f3d0fcee68458e4c309b576ccbfb14
Andrew Newland <andrew.newland@icarehealth.com>
2016-02-20 20:19:47 +0000

    Added extra interfaces to common to allow admin views to send data to jobs to be processed and retrieve data to re-render.

---
+++ src/dashing.net.common/IDataProcessor.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IDataProcessor.cs" company="iCareHealth (UK) Ltd">
+//   Copyright (c) iCareHealth (UK) Ltd. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace dashing.net.common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDataProcessor
+    {
+        /// <summary>Processes the data.</summary>
+        /// <param name="data">The data.</param>
+        void ProcessData(string widgetId, IDictionary<string, object> data);
+
+        /// <summary>Retuns a string array of the Widget Ids this processor is capable of processing.</summary>
+        /// <value>The processor widget ids.</value>
+        string[] ProcessorWidgetIds { get; }
+    }
+}
---
+++ src/dashing.net.common/IDataRetriever.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dashing.net.common
+{
+    public interface IDataRetriever
+    {
+        /// <summary>Retuns a string array of the Widget Ids this processor is capable of processing.</summary>
+        /// <value>The processor widget ids.</value>
+        string[] ProcessorWidgetIds { get; }
+
+        // gets your json data to send back to the user
+        object GetData(string widgetId);
+    }
+}
