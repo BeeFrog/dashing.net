09eedccbc0f0cdf22e4d0314fce7ca9ae2d82e9f
Andrew Newland <andrew.newland@icarehealth.com>
2016-02-20 20:58:50 +0000

    Added in a scrum job which can be controlled from the admin section. This updates the burn down and sprint goal.

---
+++ src/dashing.net.jobs.Scrum/DataPersister.cs
@@ -0,0 +1,57 @@
+﻿namespace dashing.net.jobs.Scrum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DataPersister
+    {
+        public string Get(string key)
+        {
+            var path = this.GetPath(key);
+            if (path !=null && System.IO.File.Exists(path))
+            {
+                var data = File.ReadAllText(path);
+                return data;
+            }
+
+            return string.Empty;
+        }
+
+        public void Set(string key, string data)
+        {
+            var path = this.GetPath(key);
+            if (path != null)
+            {
+                File.WriteAllText(path, data);
+            }
+        }
+
+        protected string GetPath(string key)
+        {
+            // System.Web.HttpContext.Current?.Server?.MapPath("~/App_Data");
+            var folder = DllFolder(); 
+            if (folder != null)
+            {
+                var path = System.IO.Path.Combine(folder, "scrum-" + key);
+                return path;
+            }
+            return null;
+        }
+
+        protected string DllFolder()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
+        }
+}
+}
---
+++ src/dashing.net.jobs.Scrum/Job.cs
@@ -0,0 +1,128 @@
+﻿using dashing.net.common;
+using dashing.net.streaming;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dashing.net.jobs.Scrum
+{
+    [Export(typeof(IJob))]
+    public class Job : IJob, IDataProcessor, IDataRetriever
+    {
+        private DataPersister persister;
+
+        public Lazy<Timer> Timer { get; private set; }
+
+        public string[] ProcessorWidgetIds
+        {
+            get { return new string[] { "burndown", "sprintgoal" }; }
+        }
+
+        public Job() : this(new DataPersister())
+        {
+        }
+
+        public Job(DataPersister persister)
+        {
+            this.persister = persister;
+            Timer = new Lazy<Timer>(() => new Timer(SendMessage, null, TimeSpan.Zero, TimeSpan.FromMinutes(1)));
+        }
+
+        public void SendMessage(object message)
+        {
+            var sprintGoal = this.persister.Get("sprintgoal");
+            if (!string.IsNullOrWhiteSpace(sprintGoal))
+            {
+                Dashing.SendMessage(new { id = "sprintgoal", text = sprintGoal });
+            }
+
+            // Now send the burndown
+            var data = this.GetData("burndown");
+            if (data != null)
+            {
+                Dashing.SendMessage(new { id = "burndown", points = data });
+            }
+
+        }
+
+        public void ProcessData(string widgetId, IDictionary<string, object> data)
+        {
+            //Extract the data and convert it into my object
+            switch (widgetId) {
+                case "burndown":
+                    ProcessBurnDownData(data);
+                    break;
+                case "sprintgoal":
+                    ProcessSprintGoalData(data);
+                    break;
+            }
+        }
+
+        private void ProcessSprintGoalData(IDictionary<string, object> data)
+        {
+            string sprintGoal = this.TryGetValue(data, "text");
+
+            // Persist and save the data
+            this.persister.Set("sprintgoal", sprintGoal);
+            Dashing.SendMessage(new { id = "sprintgoal", text = sprintGoal });
+        }
+
+        private string TryGetValue(IDictionary<string, object> data, string key)
+        {
+            object value;
+            data.TryGetValue(key, out value);
+            return value + "";
+        }
+
+        private void ProcessBurnDownData(IDictionary<string, object> data)
+        {
+            var points = new List<Point>();
+
+            for(int i = 1; i < 11; i++)
+            {
+                string y = this.TryGetValue(data, "d" + i);
+                int Y;
+                int.TryParse(y, out Y);
+                points.Add(new Point() { X = i, Y = Y });
+            }
+
+            // Save the data
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(points);
+            this.persister.Set("burndown", json);
+
+            // Send it
+            Dashing.SendMessage(new { id = "burndown", points = points.Select(m => new { x = m.X, y = m.Y }) });
+        }
+
+        public object GetData(string widgetId)
+        {
+            var json = this.persister.Get(widgetId);
+
+            
+            if (!string.IsNullOrWhiteSpace(json) && widgetId == "burndown")
+            {
+                var points = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                return points;
+            }
+            else if(widgetId == "sprintgoal")
+            {
+                return json;
+            }
+
+            return null;
+        }
+
+        class Point
+        {
+            [Newtonsoft.Json.JsonProperty("x")]
+            public int X { get; set; }
+
+            [Newtonsoft.Json.JsonProperty("y")]
+            public int Y { get; set; }
+        }
+    }
+}
